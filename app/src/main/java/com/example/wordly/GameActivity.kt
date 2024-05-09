f9395407f6387afaea86a7c50105c936abbe25ca
Mixail <nemkevich.mikhail05@gmail.com>
2024-05-09 20:22:22 +0300

    +

--- app/src/main/java/com/example/wordly/GameActivity.kt
+++ app/src/main/java/com/example/wordly/GameActivity.kt
@@ -1,6 +1,8 @@
 package com.example.wordly
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,24 @@ class GameActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val backButton: ImageView = findViewById(R.id.back_view)
+        val helpButton: ImageView = findViewById(R.id.help_view_game)
+        val settingButton: ImageView = findViewById(R.id.setting_view_game)
+
+        helpButton.setOnClickListener{
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
+        }
+
+        backButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingButton.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
